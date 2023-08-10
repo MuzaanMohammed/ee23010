e957d60b40d5b81f7bb52f7cb6ab084a0def2425
MuzaanMohammed <muzaanfaizel@gmail.com>
2023-08-10 17:22:35 +0530

    Changes

--- assign1/codes/assign.py
+++ assign1/codes/assign.py
@@ -1,47 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sympy
-
-A = np.array([3.0, 4.0])  # Use floating-point numbers
+import matplotlib.image as mpimg
+A = np.array([1.0, -1.0])  # Use floating-point numbers
 B = np.array([-4.0, 6.0])  # Use floating-point numbers
-C = np.array([-3.0, -5.0])  # Use floating-point numbers
-#returning coefficients of the perpendicular bisector and constant 
+C = np.array([-3.0, -5.0])  # Use floating-point numbers 
+# Midpoint of each line
+def midpoint(P, Q):
+    return (P + Q) / 2  
+#normal vector 
+def norm_vec(A,B):
+  omat = np.array([[0,1],[-1,0]]) 
+  return omat.T@(A-B)
+#to find the coefficients and constant of the equation of perpendicular bisector of BC
 def perpendicular_bisector(B, C):
-    midpoint = (B + C) / 2
-    direction = B - C
-    
-    
-   
-    constant = direction.T @ midpoint
-
-    equation_coeffs = np.concatenate((direction, [-constant]))
-    return equation_coeffs
-
-
-
-equation_coeff1 = perpendicular_bisector(A, B)
-equation_coeff2 = perpendicular_bisector(B, C)
-equation_coeff3 = perpendicular_bisector(C, A)
-
-print(f'Equation coefficients for AB: {equation_coeff1}')
-print(f'Equation for AB: ({equation_coeff1[0]:.2f})x + ({equation_coeff1[1]:.2f})y + ({equation_coeff1[2]:.2f}) = 0')
-
-print(f'Equation coefficients for BC: {equation_coeff2}')
-print(f'Equation for BC: ({equation_coeff2[0]:.2f})x + ({equation_coeff2[1]:.2f})y + ({equation_coeff2[2]:.2f}) = 0')
-
-print(f'Equation coefficients for CA: {equation_coeff3}')
-print(f'Equation for CA: ({equation_coeff3[0]:.2f})x + ({equation_coeff3[1]:.2f})y + ({equation_coeff3[2]:.2f}) = 0')
-
-
-#To find the intersection of the perpendicular bisectors
-# Coefficient Matrix
-M = np.array([equation_coeff1, equation_coeff2])
-
-# Row reduced echelon form
-res,pivots=sympy.Matrix(M).rref()
-value1 = -res[0, 2]  # x value
-value2 = -res[1, 2]  # y value
-
+    midBC=midpoint(B,C)
+    dir=B-C
+    constant = -dir.T @ midBC
+    return dir,constant
+equation_coeff1,const1 = perpendicular_bisector(A, B)
+equation_coeff2,const2 = perpendicular_bisector(B, C)
+equation_coeff3,const3 = perpendicular_bisector(C, A)
+print(f'Equation for perpendicular bisector of AB: ({equation_coeff1[0]:.2f})x + ({equation_coeff1[1]:.2f})y + ({const1:.2f}) = 0')
+print(f'Equation for perpendicular bisector of  BC: ({equation_coeff2[0]:.2f})x + ({equation_coeff2[1]:.2f})y + ({const2:.2f}) = 0')
+print(f'Equation for perpendicular bisector of  CA: ({equation_coeff3[0]:.2f})x + ({equation_coeff3[1]:.2f})y + ({const3:.2f}) = 0')
+#circumcentre of triangle ABC
+def ccircle(A,B,C):
+  p = np.zeros(2)
+  n1 = equation_coeff1[:2]
+  p[0] = 0.5*(np.linalg.norm(A)**2-np.linalg.norm(B)**2)
+  n2 = equation_coeff2[:2]
+  p[1] = 0.5*(np.linalg.norm(B)**2-np.linalg.norm(C)**2)
+  #Intersection
+  N=np.block([[n1],[n2]])
+  O=np.linalg.solve(N,p)
+  return O
+O=ccircle(A,B,C)
 # Generate points along a line
 def line_gen(A, B):
     len = 10
@@ -52,21 +45,14 @@ def line_gen(A, B):
         temp1 = A + lam_1[i] * (B - A)
         x_AB[:, i] = temp1.T
     return x_AB
-
 # Generating all lines
 x_AB = line_gen(A, B)
 x_BC = line_gen(B, C)
 x_CA = line_gen(C, A)
-
 # Plotting all lines
 plt.plot(x_AB[0, :], x_AB[1, :], label='$AB$')
 plt.plot(x_BC[0, :], x_BC[1, :], label='$BC$')
 plt.plot(x_CA[0, :], x_CA[1, :], label='$CA$')
-
-# Midpoint of each line
-def midpoint(P, Q):
-    return (P + Q) / 2
-
 # Perpendicular bisector
 def line_dir_pt(m, A, k1=0, k2=1):
     len = 10
@@ -77,48 +63,45 @@ def line_dir_pt(m, A, k1=0, k2=1):
         temp1 = A + lam_1[i] * m
         x_AB[:, i] = temp1.T
     return x_AB
-
 # Calculate the perpendicular vector and plot arrows
 def perpendicular(B, C, label):
-    BC_direction = B - C
-    perpendicular = np.array([-BC_direction[1], BC_direction[0]])
+    perpendicular=norm_vec(B,C)
     mid = midpoint(B, C)
     x_D = line_dir_pt(perpendicular, mid, 0, 1)
     plt.arrow(mid[0], mid[1], perpendicular[0], perpendicular[1], color='blue', head_width=0.4, head_length=0.4, label=label)
     plt.arrow(mid[0], mid[1], -perpendicular[0], -perpendicular[1], color='blue', head_width=0.4, head_length=0.4)
     return x_D
-
 x_D = perpendicular(A, B, 'OD')
 x_E = perpendicular(B, C, 'OE')
 x_F = perpendicular(C, A, 'OF')
-
-
-
-
-
-
-# Label the triangle vertices and intersection point
-
-mid12 = midpoint(A, B)
-mid23 = midpoint(B, C)
-mid31 = midpoint(C, A)
-intersection = np.array([value1,value2])
-
-
-points = {'A': A, 'B':B, 'C': C, 'D': mid12, 'E': mid23, 'F': mid31,'O':intersection }
-for label, point in points.items():
-  plt.annotate(label, point, textcoords="offset points", xytext=(0, 10), ha='center', fontsize=12)
-  #labelling the 90 degree angle
-  if label in ['D', 'E', 'F']:
-    plt.text(point[0], point[1], ' L90°', fontsize=12, color='black', ha='center', va='center')
-
-
+mid1 = midpoint(A, B)
+mid2 = midpoint(B, C)
+mid3 = midpoint(C, A)
+#Labeling the coordinates
+#tri_coords = np.vstack((A,B,C,O,I)).T
+#np.block([[A1,A2,B1,B2]])
+A = A.reshape(-1,1)
+B = B.reshape(-1,1)
+C = C.reshape(-1,1)
+O = O.reshape(-1,1)
+mid12=mid1.reshape(-1,1)
+mid23=mid2.reshape(-1,1)
+mid31=mid3.reshape(-1,1)
+tri_coords = np.block([[A,B,C,O,mid12,mid23,mid31]])
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','C','O','D','E','F']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
 plt.xlabel('$x$')
 plt.ylabel('$y$')
 plt.legend(loc='best')
-plt.grid()
+plt.grid() # minor
 plt.axis('equal')
-plt.show()
+plt.savefig("trianglecircum.png")
 
 
 
