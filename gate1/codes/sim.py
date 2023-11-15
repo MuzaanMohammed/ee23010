5e985e58052d90f10cc58ba02cb13f0dd4fb0346
MuzaanMohammed <muzaanfaizel@gmail.com>
2023-11-15 17:11:16 +0530

    commij333

--- gate1/codes/sim.py
+++ gate1/codes/sim.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Given mean
-mean = float(input("Enter a mean value : "))
+mean = float(input("Enter a mean value: "))
 
 # Number of trials for binomial distribution
 trials = 100
@@ -11,7 +11,7 @@ trials = 100
 probabilities = np.linspace(0.01, 0.99, 100)
 
 # Calculate variances for each probability using binomial random variable
-variances = []
+variances_binomial = []
 
 for p in probabilities:
     # Generate binomial random variable
@@ -21,25 +21,29 @@ for p in probabilities:
     variance = np.var(binomial_rv)
     
     # Append to the list
-    variances.append(variance)
+    variances_binomial.append(variance)
 
-# Find minimum and maximum of the variance array
-min_variance = np.min(variances)
-max_variance = np.max(variances)
+# Calculate theoretical variances using the formula variance = mean * (1 - p)
+variances_theoretical = mean * (1 - probabilities)
 
+# Find minimum and maximum of the binomial variance array
+min_variance = np.min(variances_binomial)
+max_variance = np.max(variances_binomial)
 
 # Get input variance from the user
 user_input = float(input("Enter a variance value to check: "))
 
 # Check if the input variance is within the range
 if min_variance <= user_input < max_variance:
-    print("valid")
+    print("Valid")
 else:
-    print("invalid")
+    print("Invalid")
 
-# Plotting the variance as a function of probability
-plt.plot(probabilities, variances)
+# Plotting the variances
+plt.plot(probabilities, variances_binomial, label='Binomial Distribution')
+plt.plot(probabilities, variances_theoretical, label='Theoretical Distribution', linestyle='--')
 plt.xlabel('Probability (p)')
 plt.ylabel('Variance')
 plt.title('Variance vs Probability for Binomial Distribution')
+plt.legend()
 plt.show()
